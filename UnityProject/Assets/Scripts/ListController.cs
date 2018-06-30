2c4a98863015405bb4b768fd6104a74778771471
JACKS-LAPTOP\Jack <nanotek2711@yahoo.com>
2018-06-29 21:15:12 -0400

    Created prefabs out of the list and the items

---
+++ UnityProject/Assets/Scripts/ListController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListController : MonoBehaviour {
+    [SerializeField] private GameObject listItemPrefab;
+    [SerializeField] private Transform listContainer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
